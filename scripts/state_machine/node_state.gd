44507fb60e825ef1bb750cf05d9c53b7d069a9fc
luckyCheng <2862722468@qq.com>
2025-12-08 22:32:04 +0800

    小鸡模型

--- scripts/state_machine/node_state.gd
+++ scripts/state_machine/node_state.gd
@@ -8,11 +8,6 @@ signal transition
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
 func _on_physics_process(delta:float) -> void:
 	pass
 	
